6a9889a4ad0d699b48dd0526534bdaa5769557ce
Vova <tsv19su@gmail.com>
2022-11-06 09:43:52 +0300

    Правки 6 ноября 2022 года

--- SQL & XML (XPath & XQuery XSD XDR XSLT)/AirPortsAndRoutes/SQLQuerySelectAirRouteWithJOIN.sql
+++ SQL & XML (XPath & XQuery XSD XDR XSLT)/AirPortsAndRoutes/SQLQuerySelectAirRouteWithJOIN.sql
@@ -1,7 +1,7 @@
 /*  Маршруты аэропорта  */
 SET Transaction Isolation Level Repeatable Read
 DECLARE @AirPortIATA nchar(10)
-SET @AirPortIATA = 'LAX'  -- Код IATA исходного аэропорта - 300
+SET @AirPortIATA = 'DEN'  -- Код IATA исходного аэропорта - 300
 SELECT AirPortsAndRoutesDBNew62.dbo.AirPortsTable.AirPortName AS DEPARTURE,
 	   AirPortsAndRoutesDBNew62.dbo.AirPortsTable.AirPortCity AS DEPARTURE_CITY,
 	   AirPortsAndRoutesDBNew62.dbo.AirPortsTable.AirPortCountry AS DEPARTURE_COUNTRY,
@@ -14,7 +14,7 @@ FROM AirPortsAndRoutesDBNew62.dbo.AirRoutesTable INNER JOIN
      AirPortsAndRoutesDBNew62.dbo.AirPortsTable ON AirPortsAndRoutesDBNew62.dbo.AirRoutesTable.AirPortDeparture = AirPortsAndRoutesDBNew62.dbo.AirPortsTable.AirPortUniqueNumber INNER JOIN
      AirPortsAndRoutesDBNew62.dbo.AirPortsTable AS AirPortsTable_1 ON AirPortsAndRoutesDBNew62.dbo.AirRoutesTable.AirPortArrival = AirPortsTable_1.AirPortUniqueNumber
 WHERE (AirPortsAndRoutesDBNew62.dbo.AirPortsTable.AirPortCodeIATA = @AirPortIATA) OR (AirPortsTable_1.AirPortCodeIATA = @AirPortIATA)
-ORDER BY DEPARTURE, ARRIVAL
+ORDER BY DEPARTURE_CITY, ARRIVAL_CITY
 -- XSD, TPH - Tonopah
 -- пусто (KXTA) - Homey (Area 51)
 -- HMN - Holloman Air Force Base
