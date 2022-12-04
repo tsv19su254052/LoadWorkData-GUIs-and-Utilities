25e97fa302bfb70f26aed4c50f79ef1d28d60c4c
Vova <tsv19su@gmail.com>
2022-12-05 00:58:37 +0300

    Правки 05 декабря 2022 года

--- External Repos (manual added Folders)/Saxon-Forms (Saxonica)/demos/php/Test.php
+++
@@ -1,5 +0,0 @@
-<?php
-	header("Content-Type: text/plain; charset=UTF-8");
-	print_r($_GET);
-
-?>
--- External Repos (manual added Folders)/Saxon-Forms (Saxonica)/samples/sample3/php/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-	header("Content-Type: application/xml; charset=UTF-8");
-	echo "<test>Results from test.php</test>";
-
-?>
