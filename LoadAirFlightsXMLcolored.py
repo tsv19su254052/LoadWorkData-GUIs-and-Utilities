9723b4ccea04ebef39ec8e78ce6c2cf60c8b23d6
Sergey <tsv19su@yandex.ru>
2024-02-25 13:16:37 +0300

    Правки 25 феврала 2024 года

--- LoadAirFlightsXMLcolored.py
+++ LoadAirFlightsXMLcolored.py
@@ -1104,7 +1104,7 @@ def myApplication():
         myDialog.pushButton_Disconnect_FN.setEnabled(False)
         myDialog.pushButton_Disconnect_AC.setEnabled(False)
         myDialog.pushButton_GetStarted.setEnabled(False)
-        # todo Заброс на возможность запуска нескольких загрузок с доработкой графической оболочки
+        # todo Заброс на возможность запуска нескольких загрузок с доработкой графической оболочки без ее закрытия на запуске загрузки
         threadLoad = threading.Thread(target=LoadThread, daemon=False, args=(S.InputFileCSV, S.LogFileTXT, ))  # поток не сам по себе
         threadLoad.start()
         # fixme с ... .join() кнопки не гаснут, графическая оболочка зависает -> убрал ... .join()
